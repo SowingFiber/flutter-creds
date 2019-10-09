79fbd8d6b10f21e8b381b0c82c7a50a50e7df067
SowingFiber <iamshanuuknow@outlook.com>
2019-10-09 08:10:22 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              onPressed: () {},
+              child: Text('Sign in'),
+            ),
+            FlatButton(
+              onPressed: () {},
+              child: Text('Sign up'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
